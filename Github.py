166250f747699bacdb3ba56aac69c638e61f2c8a
new92 <94779840+new92@users.noreply.github.com>
2022-04-16 19:04:03 +0300

    Create Github.py

---
+++ Github.py
@@ -0,0 +1,24 @@
+#Imports: 
+
+import sys
+import urllib.parse
+import webbrowser
+
+#End of imports
+
+#Program: 
+
+def main(args):
+    def quote(arg):
+        if ' ' in arg:
+            arg = '"%s"' % arg
+        return urllib.parse.quote_plus(arg)
+
+    qstring = '+'.join(quote(arg) for arg in args)
+    url = urllib.parse.urljoin('https://github.com/', ''+ qstring)
+    webbrowser.open(url)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
+#End of the program 
